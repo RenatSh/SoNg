a274c083c629c60e83427a03ce4d3c97f9f00363
Renat Shayhutdinov <Renat.Shayhutdinov@icl-services.com>
2019-06-23 22:35:29 +0300

    added bootstrap

--- ng7-so/src/app/stack-exchange-api.service.ts
+++ ng7-so/src/app/stack-exchange-api.service.ts
@@ -1,26 +1,56 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+// @Injectable({
+//   providedIn: 'root'
+// })
+
+export interface SearchResult {
+    items:           Item[];
+    has_more:        boolean;
+    quota_max:       number;
+    quota_remaining: number;
+}
+
+export interface Item {
+    tags:               string[];
+    owner:              Owner;
+    is_answered:        boolean;
+    view_count:         number;
+    answer_count:       number;
+    score:              number;
+    last_activity_date: number;
+    creation_date:      number;
+    last_edit_date?:    number;
+    question_id:        number;
+    link:               string;
+    title:              string;
+}
+
+export interface Owner {
+    reputation:    number;
+    user_id:       number;
+    user_type:     string;
+    profile_image: string;
+    display_name:  string;
+    link:          string;
+    accept_rate?:  number;
+}
+
 export class StackExchangeApiService {
 
   constructor(private http: HttpClient) { }
 
-  
-  searchUrl: string = "https://api.stackexchange.com/2.2/search?pagesize=100&order=desc&sort=activity&intitle=";
+  searchUrl = 'https://api.stackexchange.com/2.2/search?pagesize=100&order=desc&sort=activity&intitle=';
 
-  getSearchResults(searchQuery: string): Observable<searchResultsFromAPI[]> {
-      return this.http.get<{items: any[]}>(
-        `${this.searchUrl}${searchQuery}&site=stackoverflow`
-        ).map(data=>data.items);
+  getSearchResults(searchQuery: string) {
+      return this.http.get<SearchResult>(
+        `${this.searchUrl}${searchQuery}&site=stackoverflow`);
+        // ).map(data=>data.items);
         //encodeURIComponent
   }
 
   /*
-  
-
 export const searchSOByUserUrl = function (userId) {
     let url = "http://api.stackexchange.com/2.2/users/"
         + userId
